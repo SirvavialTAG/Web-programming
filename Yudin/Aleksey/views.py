a7bdcb806fc79f08bfe2f16d341e5189168a0953
SirvavialTAG <123053453+SirvavialTAG@users.noreply.github.com>
2024-10-15 23:35:33 +1000

    Замена файла views.py

--- Yudin/Aleksey/views.py
+++ Yudin/Aleksey/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
@@ -9,16 +8,16 @@ def index(request):
 
 
 def name(request):
-    return HttpResponse("<h2>Юдин Алексей</h2>")
+    return render(request, 'name.html')
 
 
 def age(request):
-    return HttpResponse("<h2>20 лет</h2>")
+    return render(request, 'age.html')
 
 
 def groupe(request):
-    return HttpResponse("<h2>БИН-22-2</h2>")
+    return render(request, 'groupe.html')
 
 
 def common(request):
-    return HttpResponse("<h2>Юдин Алексей, 20 лет, БИН-22-2</h2>")
+    return render(request, 'common.html')
